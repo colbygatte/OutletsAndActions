6d09127870ce6d4edffb68d2809d8796f1867aaf
colbygatte <colbygatte@gmail.com>
2016-09-26 13:08:10 -0500

    Outlets and Actions
    First assignment for iOS course

---
+++ ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  OutletsAndActions
+//
+//  Created by Colby Gatte on 9/26/16.
+//  Copyright © 2016 Colby Gatte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UITextField *firstNameTextField;
+@property (nonatomic, weak) IBOutlet UITextField *lastNameTextField;
+@property (nonatomic, weak) IBOutlet UILabel  *firstNameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *lastNameLabel;
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,44 @@
+//
+//  ViewController.m
+//  OutletsAndActions
+//
+//  Created by Colby Gatte on 9/26/16.
+//  Copyright © 2016 Colby Gatte. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(IBAction) submitButtonClicked {
+    NSString *firstName = self.firstNameTextField.text;
+    NSString *lastName = self.lastNameTextField.text;
+    
+    NSString *welcomeMessage = [NSString stringWithFormat:@"%@ %@",firstName,lastName];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Message" message:welcomeMessage preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
